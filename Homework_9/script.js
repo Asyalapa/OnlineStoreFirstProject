9315183ff3d1c346b7469d50479218e4418eae4d
Анастасия Макарова <asyalapa@mail.ru>
2021-11-25 11:08:36 +0300

    started hw 9

---
+++ Homework_9/script.js
@@ -0,0 +1,38 @@
+'use strict'
+
+// Решение без *-------------------------------------------------------
+
+// Вариант 1
+
+// function makeFibonacciFunction(firstNumber = 1, secondNumber = 0) {
+//   return function сalculatFibonacciNumber() {
+//     secondNumber += firstNumber;
+//     firstNumber = secondNumber - firstNumber;
+//     return secondNumber
+//   }
+// }
+
+// Вариант 2
+
+// function makeFibonacciFunction(firstNumber = 1, secondNumber = 0) {
+//   return () => {
+//     secondNumber += firstNumber;
+//     firstNumber = secondNumber - firstNumber;
+//     return secondNumber
+//   }
+// }
+
+// const fibonacci = makeFibonacciFunction()
+
+// Решение со *----------------------------------------------------------
+
+const fibonacci = ((firstNumber = 1, secondNumber = 0) => () => {
+    secondNumber += firstNumber;
+    firstNumber = secondNumber - firstNumber;
+    return secondNumber;
+    })();
+
+// Вывод в консоль--------------------------------------------------------
+while (confirm('Проведем итерацию?')) {
+  console.log(fibonacci() );
+}
