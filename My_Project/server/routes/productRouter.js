5790be932931cdc9d6b12a853d68a5be074cf54a
asyalapa <Asya2908>
2021-12-02 18:09:21 +0300

    Создали файлы-контроллеры, и прописали шаблон логики исполнения запросов.

--- My_Project/server/routes/productRouter.js
+++ My_Project/server/routes/productRouter.js
@@ -1,10 +1,10 @@
 const Router = require('express')
 const router = new Router()
-// const productController = require('../controllers/productController')
+const productController = require('../controllers/productController')
 
-// router.post('/', productController.create)
-// router.get('/', productController.getAll)
-// router.get('/:id', productController.getOne)
-// router.delete('/:id', productController.delete)
+router.post('/', productController.create)
+router.get('/:id', productController.getOne)
+router.get('/', productController.getAll)
+router.delete('/:id', productController.delete)
 
 module.exports = router
