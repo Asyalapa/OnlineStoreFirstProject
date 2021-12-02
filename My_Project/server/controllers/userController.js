8f2755f41a0497e6ad1ec3e3e94caed7b058944d
asyalapa <Asya2908>
2021-12-02 18:46:51 +0300

    Добавили middleware который будет обрабатывать все ошибки

--- My_Project/server/controllers/userController.js
+++ My_Project/server/controllers/userController.js
@@ -1,3 +1,5 @@
+const apiError = require('../error/apiError')
+
 class UserController {
     async registration (req, res) {
 
@@ -5,9 +7,12 @@ class UserController {
     async login (req, res) {
 
     }
-    async check (req, res) {
-        // const query = req.query
-        // res.json(query)
+    async check (req, res, next) {
+        const {id} = req.query
+        if (!id) {
+            return next(apiError.badRequest('Не задан ID'))
+        }
+        res.json(id)
     }
 }
 
