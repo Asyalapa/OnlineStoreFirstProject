4bd1579db5cce41c1294455320517b9f2a0f87dd
asyalapa <Asya2908>
2021-12-07 12:49:45 +0300

    working

--- My_Project/server/controllers/productController.js
+++ My_Project/server/controllers/productController.js
@@ -6,7 +6,7 @@ const path = require('path');
 class ProductController {
     async create(req, res, next) {
         try {
-            const {name, price, categoryId, sizeId, info} = req.body
+            let {name, price, categoryId, sizeId, info} = req.body
             const {img} = req.files
             let fileName = uuid.v4() + '.jpg'
             img.mv(path.resolve(__dirname, '..', 'static', fileName))
